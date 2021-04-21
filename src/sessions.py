ac380c4c41f542ed23d3d7d787b21b6c359f1039
Ted <teddsbennett@gmail.com>
2021-04-21 23:57:30 +0100

    Add session endpoints

--- src/sessions.py
+++ src/sessions.py
@@ -13,3 +13,21 @@ def get_session_by_key(event, context):
         'statusCode': 200,
         'body': json.dumps('Got Session by Key!')
     }
+
+def create_session(event, context):
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Create session!')
+    }
+
+def update_session(event, context):
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Update session!')
+    }
+
+def delete_session(event, context):
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Delete session!')
+    }
