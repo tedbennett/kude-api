464c1d42b5556f5b4fc700c41df14cb33c9812df
Ted <teddsbennett@gmail.com>
2021-04-28 22:06:50 +0100

    Fix s3 query

--- src/s3.py
+++ src/s3.py
@@ -1,20 +1,22 @@
 import boto3
+import os
 from error import ApiError
-from utils import _extract_body, _process_api_error, _success_response
+from utils import _extract_query, _process_api_error, _success_response
 
 
 def get_s3_signed_url(event, context):
     try:
         s3_client = boto3.client('s3')
-        body = _extract_body(event)
+        user_id = _extract_query(event)['user_id']
 
-        if 'user_id' not in body:
-            raise ApiError('No user id provided', 400)
-
-        response = s3_client.generate_presigned_url(
+        signedRequest = s3_client.generate_presigned_url(
             'put_object',
-            Params={'Bucket': "kude", 'Key': "images/" + body['user_id']})
+            Params={'Bucket': "kude", 'Key': "images/" + user_id, 'Content-Type': 'jpeg'})
 
-        return _success_response(response)
+        s3_name = os.environ.get('S3_BUCKET')
+        return _success_response({
+            'signedRequest': signedRequest,
+            'url': f'https://{s3_name}.s3.amazonaws.com/images/{user_id}'
+        })
     except ApiError as e:
         return _process_api_error(e)
